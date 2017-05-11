c0ac39a10cad3e204be33f71fc5c22e13bda105e
Paula Jimenez <pjimvel@gmail.com>
2017-05-11 12:04:20 +0200

    buscador por lugar

--- ecofood/routes/index.js
+++ ecofood/routes/index.js
@@ -7,14 +7,38 @@ const Product = require('../models/product');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  // res.render('index');
-  Product
-    .find({})
-    .populate('producer')
-    .exec( (err, products) => {
-      console.log(products);
+  // console.log("AQUI!!!! " + req.body.provincia);
+    Product
+      .find({})
+      .populate('producer')
+      .exec( (err, products) => {
+        // console.log(products);
         res.render('index', { products });
-    });
+      });
 });
+
+router.post('/provincia', (req, res, next) => {
+  console.log("AQUI!!!! " + req.body.provincia);
+  const idProvincia = req.body.provincia;
+    Product
+      .find({location: idProvincia})
+      .populate('producer')
+      .exec( (err, products) => {
+        console.log(products);
+          res.render('index', {products});
+      });
+  // }
+});
+
+// router.get('/provincia', (req, res, next) => {
+//     const idProvincia = req.params.idProvincia;
+//     // Product
+//     //   .find({provincia: idProvincia})
+//     //   .populate('producer')
+//     //   .exec( (err, products) => {
+//     //     console.log(products);
+//         res.render('index', { products });
+//       // });
+// });
 
 module.exports = router;
